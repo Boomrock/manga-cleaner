af1c79cc6ae53341aef2499853da1a9dd4155f55
Boomrock <gorlopan.03@gmail.com>
2025-06-03 02:27:07 +0700

    fix

--- Preprocessor.py
+++ Preprocessor.py
@@ -2,7 +2,7 @@ import PIL
 import PIL.Image
 import numpy
 import torch
-from DetectorType import DetectorType
+from Cleaner.DetectorType import DetectorType
 from torchvision import transforms
 
 
@@ -17,4 +17,19 @@ class Preprocessor:
         image = transform(image).unsqueeze(0) # type: ignore [1,3,h,w]
         return image # type: ignore
     
+class UnetPlusPlusPreprocessor(Preprocessor):
+    def process(self, image: PIL.Image.Image) -> torch.Tensor:
+        original_size = image.size  # (width, height)
 
+        # Вычисление необходимого паддинга до кратности 32
+        def to_divisible(x, divisor=32):
+            return ((x + divisor) // divisor) * divisor
+
+        new_width = to_divisible(original_size[0])
+        new_height = to_divisible(original_size[1])
+
+        # Применение Resize и других трансформаций
+        resized_image = image.resize((new_width, new_height))
+        
+        return super().process(resized_image)
+    
